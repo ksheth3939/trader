b8a9ab5997f84a442529845b79213cf74f1402cc
Kunal Sheth <ks782@cornell.edu>
2021-10-17 01:05:47 -0400

    Switch to RSI

--- main2.py
+++ main2.py
@@ -18,9 +18,9 @@ def trade():
 
   start_date = '20' + (dt.datetime.now() - dt.timedelta(days = 60)).strftime(('%y-%m-%d'))
   # print(start_date)
-  rsi_owned = []
-  with open('rsi.pkl', 'rb') as f:
-    rsi_owned = pickle.load(f)
+  stoch_owned = []
+  with open('stoch.pkl', 'rb') as f:
+    stoch_owned = pickle.load(f)
 
   with open('trades.csv', 'a') as f:
     writ = writer(f)
@@ -47,20 +47,22 @@ def trade():
           break
 
       last_row = df.iloc[-1]
-      if stoch_buy and last_row['RSI Adj'] > 50 and last_row['MACD'] > last_row['Signal'] and (comp not in owned):
+      if stoch_buy and last_row['RSI Adj'] > 50 and last_row['MACD'] > last_row['Signal'] and (comp not in stoch_owned):
         #buy
-        print('Buy',comp,last_row['Adj Close'])
-        #bought = robin.orders.order_buy_market(comp, round(50/last_row['Adj Close'],4), timeInForce='gfd')
-        #print(bought,'bought',type(bought))
-        #if bought != None and bought != {}:
-        #  writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'BUY',bought['quantity'],float(bought['quantity']) * last_row['Adj Close'],last_row['Adj Close'],'Stoch'])
-
+        cost = last_row['Adj Close']
+        writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'BUY', last_row['RSI Adj'], 0, cost,'Stoch'])
+        stoch_owned.append(comp)
         # print(last_row['Adj Close'])
         # print(bought)
-      if last_row['RSI Adj'] < 25 and last_row['RSI Adj'] > 0.0 and (comp not in rsi_owned):
-        cost = last_row['Adj Close']
-        #writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'BUY', last_row['RSI Adj'], 0, cost,'RSI'])
-        #rsi_owned.append(comp)
+
+      if last_row['RSI Adj'] < 25 and last_row['RSI Adj'] > 0.0 and (comp not in owned):
+
+        print('Buy',comp,last_row['Adj Close'])
+        bought = robin.orders.order_buy_market(comp, round(50/last_row['Adj Close'],4), timeInForce='gfd')
+        print(bought,'bought',type(bought))
+        if bought != None and bought != {}:
+          writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'BUY',bought['quantity'], float(bought['quantity']) * last_row['Adj Close'],last_row['Adj Close'],'RSI'])
+        
 
 
       stoch_sell = False
@@ -72,24 +74,27 @@ def trade():
           stoch_sell = False
           break
 
-      if (comp in owned and stoch_sell and last_row['RSI Adj'] < 50 and last_row['MACD'] < last_row['Signal']):
+      if (comp in stoch_owned and stoch_sell and last_row['RSI Adj'] < 50 and last_row['MACD'] < last_row['Signal']):
         #sell
-        print('Sell',comp,last_row['Adj Close'])
-        #sold = robin.orders.order_sell_fractional_by_quantity(comp, owned[comp]['quantity'])
-        #print(sold, 'sold',type(sold))
-        #if sold != None and sold != {}:
-        #  writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'SELL',sold['quantity'], float(sold['quantity']) * last_row['Adj Close'],last_row['Adj Close'],'Stoch'])
-
-      if last_row['RSI Adj'] > 75 and (comp in rsi_owned):
         cost = last_row['Adj Close']
-        #writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'SELL', last_row['RSI Adj'], 0, cost,'RSI'])
-        #rsi_owned.remove(comp)
+        writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'SELL', last_row['RSI Adj'], 0, cost,'Stoch'])
+        stoch_owned.remove(comp)
+        
+      if last_row['RSI Adj'] > 75 and (comp in owned):
+
+        print('Sell',comp,last_row['Adj Close'])
+        sold = robin.orders.order_sell_fractional_by_quantity(comp, owned[comp]['quantity'])
+        print(sold, 'sold',type(sold))
+        if sold != None and sold != {}:
+          writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'SELL',sold['quantity'], float(sold['quantity']) * last_row['Adj Close'],last_row['Adj Close'],'RSI'])
+
+        
 
   
 
     robin.logout()
 
   with open('rsi.pkl', 'wb') as f:
-    pickle.dump(rsi_owned,f)
+    pickle.dump(stoch_owned,f)
 
   f.close()
