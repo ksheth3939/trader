f22d9ec9e96f77e0f32780b511cb47f1f6153c5a
Kunal Sheth <ks782@cornell.edu>
2021-10-17 01:20:28 -0400

    Remove stoch holdings

--- test.py
+++ test.py
@@ -1,12 +1,34 @@
+import robin_stocks.robinhood as robin
+import datetime as dt
+import credentials
+import pyotp
 import pickle
+import yfinance as yf
+from csv import writer
 
+totp = pyotp.TOTP(credentials.mfa).now()
+robin.login(credentials.username, credentials.password, mfa_code = totp)
+owned = robin.build_holdings()
+start_date = '20' + (dt.datetime.now() - dt.timedelta(days = 60)).strftime(('%y-%m-%d'))
 
-# x = []
-with open('sample.pkl', 'rb') as f:
-  x = pickle.load(f)
-  print('x',x)
-  x.append(100)
-# x = list(range(10))
+stoch_owned = []
+with open('stoch.pkl', 'rb') as f:
+  stoch_owned = pickle.load(f)
 
-with open('sample.pkl', 'wb') as f:
-  pickle.dump(x,f)
+with open('trades.csv', 'a') as f:
+  writ = writer(f)
+
+for comp in stoch_owned:
+
+  df = yf.download(tickers= comp, start= start_date, interval="1h", threads=False).reset_index()
+
+  sold = robin.orders.order_sell_fractional_by_quantity(comp, owned[comp]['quantity'])
+  print(sold, 'sold',type(sold))
+
+  if sold != None and sold != {}:
+    writ.writerow([dt.datetime.now() - dt.timedelta(hours=4), comp, 'SELL',sold['quantity'], float(sold['quantity']) * last_row['Adj Close'],last_row['Adj Close'],'RSI'])
+
+        
+
+with open('stoch.pkl', 'wb') as f:
+  pickle.dump([],f)
