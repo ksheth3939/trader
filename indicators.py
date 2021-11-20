ce038359700c7802cf32c99738e046dd15da77d6
Kunal Sheth <ks782@cornell.edu>
2021-11-20 01:19:23 -0500

    Cleaned up code

--- indicators.py
+++ indicators.py
@@ -1,33 +1,59 @@
 import numpy as np
 import pandas as pd
 
-def RSI_Adj(df,n): 
+def RSI_Adj(df,n, comp = None): 
   def rma(x, n, y0):
     a = (n-1) / n
     ak = a**np.arange(len(x)-1, -1, -1)
     return np.r_[np.full(n, np.nan), y0, np.cumsum(ak * x) / ak / n + y0 * a**np.arange(1, len(x)+1)]
 
-  change = df['Adj Close'].diff()
+  if not comp:
+    change = df['Adj Close'].diff()
+  else:
+    change = df[comp]['Adj Close'].diff()
+  
   gain = change.mask(change < 0, 0.0)
   loss = -change.mask(change > 0, -0.0)
   avg_gain = rma(gain[n+1:].to_numpy(), n, np.nansum(gain.to_numpy()[:n+1])/n)
   avg_loss = rma(loss[n+1:].to_numpy(), n, np.nansum(loss.to_numpy()[:n+1])/n)
-  # df['rs'] =
   avg_loss =np.where(avg_loss == 0, 0.000001, avg_loss)
   # if avg_loss == 0:
   #   df['RSI Adj'] = 100
   # else:
-  df['RSI Adj'] = 100 - (100 / (1 + (avg_gain / avg_loss)))
+  if not comp:
+    df['RSI Adj'] = 100 - (100 / (1 + (avg_gain / avg_loss)))
+  else:
+    df.loc[:, (comp, 'RSI Adj')] = 100 - (100 / (1 + (avg_gain / avg_loss)))
   # return df
 
-def MACD(df, fast, slow, signal):
-  ex1 = df['Adj Close'].ewm(span = fast, adjust = False).mean()
-  ex2 = df['Adj Close'].ewm(span = slow, adjust = False).mean()
-  df['MACD'] = ex1 - ex2
-  df['Signal'] = df['MACD'].ewm(span = signal, adjust = False).mean()
+def MACD(df, fast, slow, signal, comp = None):
+  if not comp:
+    ex1 = df['Adj Close'].ewm(span = fast, adjust = False).mean()
+    ex2 = df['Adj Close'].ewm(span = slow, adjust = False).mean()
+    df['MACD'] = ex1 - ex2
+    df['Signal'] = df['MACD'].ewm(span = signal, adjust = False).mean()
 
-def Stochastic(df, w, m):
-  high14 = df['High'].rolling(window = w).max()
-  low14 = df['Low'].rolling(window = w).min()
-  df['%K'] = 100*(df['Adj Close'] - low14)/(high14 - low14)
-  df['%D'] = df['%K'].rolling(window = m).mean()
+  else:
+    ex1 = df[comp]['Adj Close'].ewm(span = fast, adjust = False).mean()
+    ex2 = df[comp]['Adj Close'].ewm(span = slow, adjust = False).mean()
+    df.loc[:, (comp, 'MACD')] = ex1 - ex2
+    df.loc[:, (comp, 'Signal')] = df[comp]['MACD'].ewm(span = signal, adjust = False).mean()
+
+def Stochastic(df, w, m, comp = None):
+  if not comp:
+    high14 = df['High'].rolling(window = w).max()
+    low14 = df['Low'].rolling(window = w).min()
+    df['%K'] = 100*(df['Adj Close'] - low14)/(high14 - low14)
+    df['%D'] = df['%K'].rolling(window = m).mean()
+
+  else:
+    high14 = df[comp]['High'].rolling(window = w).max()
+    low14 = df[comp]['Low'].rolling(window = w).min()
+    df.loc[:, (comp, '%K')] = 100*(df[comp]['Adj Close'] - low14)/(high14 - low14)
+    df.loc[:, (comp, '%D')] = df[comp]['%K'].rolling(window = m).mean()   
+
+# RSI_Adj(df,14)
+# MACD(df,12,26,9)
+# Stochastic(df,14,3)
+
+# df
---
+++ main3.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import robin_stocks.robinhood as robin
+import yfinance as yf
+import datetime as dt
+import indicators
+import credentials
+import pyotp
+import pickle
+from csv import writer
+
+companies=list(pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'])
+
+def json_to_list(json):
+  tim = dt.datetime.strptime(json['begins_at'], '%Y-%m-%dT%H:%M:%SZ') - dt.timedelta(hours = 4)
+  return [json['symbol'], tim, float(json['close_price']), int(json['volume'])]
+
+def get_data(comp):
+  data = robin.stocks.get_stock_historicals(comp, interval='hour', span='3month', bounds='regular', info=None)
+  data = list(map(json_to_list, data))
+  df = pd.DataFrame(data, columns = ['Ticker','Timestamp', 'Adj Close', 'Volume'])
+  indicators.RSI_Adj(df, 14)
+  indicators.MACD(df,12,26,9)
+  indicators.Stochastic(df,14,3)
+  return df
+
+def trade():
+  totp = pyotp.TOTP(credentials.mfa).now()
+  robin.login(credentials.username, credentials.password, mfa_code = totp)
+  owned = robin.build_holdings()
+
+  for i, comp in enumerate(companies):
+
+    df = get_data(comp)
+
+    last_row = df.iloc[-1]
+
+    if last_row['RSI Adj'] < 25 and last_row['RSI Adj'] > 0.0 and (comp not in owned):
+
+      print('Buy',comp,last_row['Adj Close'])
+      bought = robin.orders.order_buy_market(comp, round(50/last_row['Adj Close'],4), timeInForce='gfd')
+      print(bought,'bought',type(bought))
+
+    if last_row['RSI Adj'] > 75 and (comp in owned):
+
+      print('Sell',comp,last_row['Adj Close'])
+      sold = robin.orders.order_sell_fractional_by_quantity(comp, owned[comp]['quantity'])
+      print(sold, 'sold',type(sold))
+
+  robin.logout()
